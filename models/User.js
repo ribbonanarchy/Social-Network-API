0e6c79a211194a0917fd90f5fb4c46680370e1f1
Robyn Arnecke <robynarnecke@gmail.com>
2022-03-17 21:12:40 -0500

    User model updated

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, 
+      match: /.+\@.+\..+/,
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId, 
+            ref: 'Thought',
+        }
+    ], 
+    friends: [
+        {
+            type: Schema.Types.ObjectId, 
+            ref: 'User', 
+        }
+    ]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
