889fdcb9a129dca478a1f77f1e15785c97adc56b
Robyn Arnecke <robynarnecke@gmail.com>
2022-03-22 00:58:06 -0500

    Updated the utils folder

---
+++ utils/data.js
@@ -0,0 +1,118 @@
+const names = [
+    'Aaran',
+    'Aaren',
+    'Aarez',
+    'Aarman',
+    'Aaron',
+    'Aaron-James',
+    'Aarron',
+    'Aaryan',
+    'Aaryn',
+    'Aayan',
+    'Aazaan',
+    'Abaan',
+    'Abbas',
+    'Abdallah',
+    'Abdalroof',
+    'Abdihakim',
+    'Abdirahman',
+    'Abdisalam',
+    'Abdul',
+    'Abdul-Aziz',
+    'Abdulbasir',
+    'Abdulkadir',
+    'Abdulkarem',
+    'Smith',
+    'Jones',
+    'Coollastname',
+    'enter_name_here',
+    'Ze',
+    'Zechariah',
+    'Zeek',
+    'Zeeshan',
+    'Zeid',
+    'Zein',
+    'Zen',
+    'Zendel',
+    'Zenith',
+    'Zennon',
+    'Zeph',
+    'Zerah',
+    'Zhen',
+    'Zhi',
+    'Zhong',
+    'Zhuo',
+    'Zi',
+    'Zidane',
+    'Zijie',
+    'Zinedine',
+    'Zion',
+    'Zishan',
+    'Ziya',
+    'Ziyaan',
+    'Zohaib',
+    'Zohair',
+    'Zoubaeir',
+    'Zubair',
+    'Zubayr',
+    'Zuriel',
+    'Xander',
+    'Jared',
+    'Courtney',
+    'Gillian',
+    'Clark',
+    'Jared',
+    'Grace',
+    'Kelsey',
+    'Tamar',
+    'Alex',
+    'Mark',
+    'Tamar',
+    'Farish',
+    'Sarah',
+    'Nathaniel',
+    'Parker',
+  ];
+  
+  const thoughts = [
+    'I should read more.', 
+    'Star Trek is better than Star Wars.', 
+    'My dog really could be a movie star.', 
+    'Tomorrow is a brand new day.', 
+    'Today is also a day.', 
+    'Yesterday is in the past.'
+  ]
+
+  const reactions = [
+    'Woah!', 
+    'That is remarkable.',
+    'Definitely going to show this to all my friends.', 
+    'Top notch.', 
+    'I\'m not impressed.', 
+    'Great content.', 
+    'So so true!'
+  ]
+  
+  // Get a random item given an array
+  const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+  
+  // Gets a random full name
+  const getRandomName = () =>
+    `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+
+  // Function to generate random assignments that we can add to student object.
+  const getRandomThoughts = (int) => {
+    const results = [];
+    for (let i = 0; i < int; i++) {
+      results.push({
+        thoughtText: getRandomArrItem(thoughts),
+        username: getRandomArrItem(names), 
+        reactions: [getRandomArrItem(thought)]
+      });
+    }
+    return results;
+  };
+  
+  // Export the functions for use in seed.js
+  module.exports = { getRandomName, getRandomThoughts };
+  
