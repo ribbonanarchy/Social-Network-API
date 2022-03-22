a9bd6c43d22f1262e05e2b2b7bf16f39c1cedebe
Robyn Arnecke <robynarnecke@gmail.com>
2022-03-21 23:41:01 -0500

    Made first update to user controller and routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    // Get all students
+    // Get all users
     getUsers(req, res) {
       User.find() 
         .then(async (users) => {
@@ -33,60 +33,49 @@ module.exports = {
           return res.status(500).json(err);
         });
     },
-    // create a new student
+    // create a new user
     createUser(req, res) {
       User.create(req.body)
         .then((user) => res.json(user))
         .catch((err) => res.status(500).json(err));
     },
-    // Delete a student and remove them from the course
-    deleteStudent(req, res) {
-      Student.findOneAndRemove({ _id: req.params.studentId })
-        .then((student) =>
-          !student
-            ? res.status(404).json({ message: 'No such student exists' })
-            : Course.findOneAndUpdate(
-                { students: req.params.studentId },
-                { $pull: { students: req.params.studentId } },
-                { new: true }
-              )
-        )
-        .then((course) =>
-          !course
-            ? res.status(404).json({
-                message: 'Student deleted, but no courses found',
-              })
-            : res.json({ message: 'Student successfully deleted' })
-        )
+    // Delete a user and remove them from the course
+    deleteUser(req, res) {
+      User.findOneAndRemove({ _id: req.params.userId })
+        .then((user) => {
+          if(!user) {
+            res.status(404).json({ message: 'No such user exists' });
+          }
+        })
         .catch((err) => {
           console.log(err);
           res.status(500).json(err);
         });
     },
   
-    // Add an assignment to a student
-    addAssignment(req, res) {
-      console.log('You are adding an assignment');
+    // Add an friend to a user
+    addFriend(req, res) {
+      console.log('You are adding a friend');
       console.log(req.body);
-      Student.findOneAndUpdate(
-        { _id: req.params.studentId },
-        { $addToSet: { assignments: req.body } },
+      User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.body } },
         { runValidators: true, new: true }
       )
-        .then((student) =>
-          !student
+        .then((user =>
+          !user
             ? res
                 .status(404)
-                .json({ message: 'No student found with that ID :(' })
-            : res.json(student)
+                .json({ message: 'No user found with that ID' })
+            : res.json(user)
         )
         .catch((err) => res.status(500).json(err));
     },
-    // Remove assignment from a student
-    removeAssignment(req, res) {
-      Student.findOneAndUpdate(
-        { _id: req.params.studentId },
-        { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
+    // Remove friend from a user
+    removeFriend(req, res) {
+      User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friend: { friendId: req.params.friendId } } },
         { runValidators: true, new: true }
       )
         .then((student) =>
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
 const {
   getUsers,
-  getSingleStudent,
-  createStudent,
-  deleteStudent,
-  addAssignment,
-  removeAssignment,
-} = require('../../controllers/studentController');
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).delete(deleteStudent);
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
 
-// /api/students/:studentId/assignments
-router.route('/:studentId/assignments').post(addAssignment);
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
 
-// /api/students/:studentId/assignments/:assignmentId
-router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(removeFriend);
 
 module.exports = router;
